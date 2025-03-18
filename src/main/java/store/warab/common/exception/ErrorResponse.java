249a2636f3dce64a9b31fdf5a708be005f81360c
kjr2020 <kjr202020@gmail.com>
2025-03-18 22:11:09 +0900

    feat(api): 회원정보 수정 페이지 api 구현

---
+++ src/main/java/store/warab/common/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package store.warab.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String path;
+}
