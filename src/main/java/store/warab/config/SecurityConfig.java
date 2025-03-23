29f621490936ca68ad5ee35f06cd05dd53d4dcce
kjr2020 <kjr202020@gmail.com>
2025-03-24 03:46:44 +0900

    fix(api): add kakao exception api

--- src/main/java/store/warab/config/SecurityConfig.java
+++ src/main/java/store/warab/config/SecurityConfig.java
@@ -92,7 +92,8 @@ public class SecurityConfig {
 
     // 경로별 인가 작업
     http.authorizeHttpRequests(
-        (auth) -> auth.requestMatchers("/").permitAll().anyRequest().authenticated());
+        (auth) ->
+            auth.requestMatchers("/", "/api/health").permitAll().anyRequest().authenticated());
 
     // 세션 설정
     http.sessionManagement(
