9902d65359900e1eb12039ce4d2f125f75d06d29
groom <hayaon0603@naver.com>
2025-03-25 16:05:41 +0900

    feat: 권한 확인 로직 추가

--- src/main/java/store/warab/controller/CommentController.java
+++ src/main/java/store/warab/controller/CommentController.java
@@ -1,19 +1,27 @@
 package store.warab.controller;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import store.warab.entity.Comment;
+import store.warab.jwt.JWTUtil;
+import store.warab.service.AuthService;
 import store.warab.service.CommentService;
 
 @RestController
 @RequestMapping("/api/v1/comment") // URL
-@CrossOrigin(origins = "http://localhost:3000") // 프론트 3000번 포트 허용
+//@CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
 
   private final CommentService commentService;
+  private final AuthService authService;
 
-  public CommentController(CommentService commentService) {
-    this.commentService = commentService;
+  public CommentController(CommentService commentService, AuthService authService) {
+
+      this.commentService = commentService;
+      this.authService = authService;
   }
 
   /** [POST] /api/v1/comment 새 댓글 생성 */
@@ -33,16 +41,27 @@ public class CommentController {
   /** [PUT] /api/comment/{commentId} 댓글 수정 */
   @PutMapping("/{commentId}")
   public Comment updateComment(
-      @PathVariable Integer commentId, @RequestBody CommentRequest request) {
-    return commentService.updateComment(commentId, request.getContent());
+      @PathVariable Integer commentId,
+      @RequestBody CommentRequest request,
+      @RequestHeader("Authorization") String token) {
+
+      Long tokenUserId = authService.extractUserId(token);
+      authService.verifyUser(tokenUserId, request.getUserId().longValue());
+
+      return commentService.updateComment(commentId, request.getContent());
   }
 
   /** [DELETE] /api/comment/{commentId} 댓글 삭제(softDelete 여부에 따라 분기) */
   @DeleteMapping("/{commentId}")
   public void deleteComment(
-      @PathVariable Integer commentId, @RequestParam(defaultValue = "false") boolean softDelete) {
-    commentService.deleteComment(commentId, softDelete);
-  }
+      @PathVariable Integer commentId,
+      @RequestParam(defaultValue = "false") boolean softDelete,
+      @RequestHeader("Authorization") String token) {
+
+      Long tokenUserId = authService.extractUserId(token);
+      commentService.deleteComment(commentId, tokenUserId, softDelete);
+
+}
 
   /** 댓글 생성/수정 시 받을 DTO */
   public static class CommentRequest {
@@ -75,3 +94,4 @@ public class CommentController {
     }
   }
 }
+
