2a240aa6bdb6fba8c1779f0498f8faf4d0e5bb44
Lim <inlim@Limui-MacBookAir.local>
2025-03-25 18:57:35 +0900

    fix: api 응답 형식을 명세서에 맞게 수정
    <type>([scope]): <commit message>

--- src/main/java/store/warab/controller/CommentController.java
+++ src/main/java/store/warab/controller/CommentController.java
@@ -1,7 +1,13 @@
 package store.warab.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import store.warab.common.util.ApiResponse;
 import store.warab.entity.Comment;
 import store.warab.service.CommentService;
 
@@ -18,30 +24,41 @@ public class CommentController {
 
   /** [POST] /api/v1/comment 새 댓글 생성 */
   @PostMapping
-  public Comment createComment(@RequestBody CommentRequest request) {
-    return commentService.createComment(
-        request.getUserId(), request.getGameId(), request.getContent());
+  public ResponseEntity<ApiResponse> createComment(@RequestBody CommentRequest request) {
+      commentService.createComment(
+          request.getUserId(), request.getGameId(), request.getContent());
+      Map<String, Object> data = null;
+      return ResponseEntity
+          .status(HttpStatus.CREATED)
+          .body(new ApiResponse("comment_create_success", data));
   }
 
   /** [GET] /api/v1/comment/game/{gameId} 특정 게임에 달린 댓글 목록 */
   @GetMapping("/game/{gameId}")
-  public List<Comment> getCommentsByGame(
+  public ResponseEntity<ApiResponse> getCommentsByGame(
       @PathVariable Integer gameId) {
-    return commentService.getCommentsByGameId(gameId);
+      List<Comment> comments = commentService.getCommentsByGameId(gameId);
+    Map<String, Object> data = new HashMap<>();
+      data.put("comments", comments);
+      return ResponseEntity.ok(new ApiResponse("comment_list inquiry_success", data));
   }
 
   /** [PUT] /api/comment/{commentId} 댓글 수정 */
   @PutMapping("/{commentId}")
-  public Comment updateComment(
+  public ResponseEntity<ApiResponse> updateComment(
       @PathVariable Integer commentId, @RequestBody CommentRequest request) {
-    return commentService.updateComment(commentId, request.getContent());
+      commentService.updateComment(commentId, request.getContent());
+      Map<String, Object> data = null;
+      return ResponseEntity.ok(new ApiResponse("update_comment_success", data));
   }
 
   /** [DELETE] /api/comment/{commentId} 댓글 삭제(softDelete 여부에 따라 분기) */
   @DeleteMapping("/{commentId}")
-  public void deleteComment(
+  public ResponseEntity<ApiResponse> deleteComment(
       @PathVariable Integer commentId, @RequestParam(defaultValue = "false") boolean softDelete) {
     commentService.deleteComment(commentId, softDelete);
+      Map<String, Object> data = null;
+      return ResponseEntity.ok(new ApiResponse("delete_comment_success", data));
   }
 
   /** 댓글 생성/수정 시 받을 DTO */
--- src/main/java/store/warab/controller/UserController.java
+++ src/main/java/store/warab/controller/UserController.java
@@ -1,5 +1,6 @@
 package store.warab.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +22,8 @@ public class UserController {
   // 로그인 구현 되면 다른 사람이 못들어오게 해야함
   @GetMapping("/profile/{userId}")
   public ResponseEntity<ApiResponse> getProfile(@PathVariable long userId) {
-    UserDto userDto = userService.getUserById(userId);
-    return ResponseEntity.ok(new ApiResponse("user_profile_inquiry_success", userDto));
+      UserDto userDto = userService.getUserById(userId);
+      return ResponseEntity.ok(new ApiResponse("user_profile_inquiry_success", userDto));
   }
 
   // 닉네임 중복 확인
