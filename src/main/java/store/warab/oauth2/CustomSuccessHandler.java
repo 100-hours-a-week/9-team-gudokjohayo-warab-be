bf6670b6dd16270bd84d0758b7cc8463a45e6483
groom <hayaon0603@naver.com>
2025-03-27 19:50:45 +0900

    fix: 쿠키 시간 수정

--- src/main/java/store/warab/oauth2/CustomSuccessHandler.java
+++ src/main/java/store/warab/oauth2/CustomSuccessHandler.java
@@ -58,7 +58,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
   private Cookie createCookie(String key, String value) {
 
     Cookie cookie = new Cookie(key, value);
-    cookie.setMaxAge(60 * 60 * 60);
+    cookie.setMaxAge(60 * 60 * 60 * 200);
     // cookie.setSecure(true);
     cookie.setPath("/");
     cookie.setHttpOnly(true);
