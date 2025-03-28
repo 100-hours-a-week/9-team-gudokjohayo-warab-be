70289b5636b38e9410986e7af0d1ff87725597b8
kjr2020 <kjr202020@gmail.com>
2025-03-28 16:48:45 +0900

    feat(cd): add deploy-common.yml

--- src/main/java/store/warab/controller/DevAuthController.java
+++ src/main/java/store/warab/controller/DevAuthController.java
@@ -2,35 +2,32 @@ package store.warab.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import store.warab.jwt.JWTUtil;
 
 @RestController
 @RequestMapping("/api/dev")
-//@Profile("dev") // 운영에선 자동 비활성화됨
+// @Profile("dev") // 운영에선 자동 비활성화됨
 public class DevAuthController {
-    private final JWTUtil jwtUtil;
+  private final JWTUtil jwtUtil;
 
-    public DevAuthController(JWTUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+  public DevAuthController(JWTUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> devLogin(
-        HttpServletResponse response) {
+  @PostMapping("/login")
+  public ResponseEntity<String> devLogin(HttpServletResponse response) {
 
-        String jwt = jwtUtil.createJwt(45L, 60 * 60 * 24L); // 1일짜리 JWT
-        Cookie cookie = new Cookie("jwt", jwt);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 24); // 1일
-        response.addCookie(cookie);
+    String jwt = jwtUtil.createJwt(45L, 60 * 60 * 24L); // 1일짜리 JWT
+    Cookie cookie = new Cookie("jwt", jwt);
+    cookie.setHttpOnly(true);
+    cookie.setPath("/");
+    cookie.setMaxAge(60 * 60 * 24); // 1일
+    response.addCookie(cookie);
 
-        return ResponseEntity.ok("개발용 JWT 쿠키 발급 완료");
-    }
+    return ResponseEntity.ok("개발용 JWT 쿠키 발급 완료");
+  }
 }
