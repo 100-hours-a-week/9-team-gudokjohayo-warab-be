77a2de7c4b429c8af8d0077ede564366d44226ce
Lim <inlim@Limui-MacBookAir.local>
2025-03-28 15:20:52 +0900

    chore: securityConfig 허용 경로에 /api/v1/auth/logout 추가

--- src/main/java/store/warab/config/SecurityConfig.java
+++ src/main/java/store/warab/config/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
     // 경로별 인가 작업
     http.authorizeHttpRequests(
         (auth) ->
-            auth.requestMatchers("/", "/api/health","/api/dev/login").permitAll().anyRequest().authenticated());
+            auth.requestMatchers("/", "/api/health","/api/dev/login","/api/v1/auth/logout").permitAll().anyRequest().authenticated());
 
     // 경로별 인가 작업
 //          http.authorizeHttpRequests(
