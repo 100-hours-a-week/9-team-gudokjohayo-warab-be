99fa2d8235528e3f8777fe7796166a38f5f50c4b
kjr2020 <kjr202020@gmail.com>
2025-04-09 16:36:04 +0900

    refactor(set): refactor applicaion.properties, application-secrets.properties

--- src/main/java/store/warab/entity/User.java
+++ src/main/java/store/warab/entity/User.java
@@ -1,6 +1,7 @@
 package store.warab.entity;
 
 import jakarta.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,7 +16,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id")
