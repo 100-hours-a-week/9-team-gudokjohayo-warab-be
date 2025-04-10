ac0507eefe161f9a2938fa0ed02ce14e89aa2884
Lim <inlim@Limui-MacBookAir.local>
2025-04-10 14:38:54 +0900

    fix: apply redis in mainPageService

--- src/main/java/store/warab/dto/GameInfoDto.java
+++ src/main/java/store/warab/dto/GameInfoDto.java
@@ -1,26 +1,33 @@
 package store.warab.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import store.warab.entity.GameDynamic;
 import store.warab.entity.GameStatic;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class GameInfoDto {
-  private Long game_id;
+  @JsonProperty("game_id")
+  private Long gameId;
+
   private String title;
   private String thumbnail;
   private Integer price;
-  private Integer lowest_price;
+
+  @JsonProperty("lowest_price")
+  private Integer lowestPrice;
 
   public GameInfoDto(GameStatic gameStatic, GameDynamic gameDynamic) {
-    this.game_id = gameStatic.getId();
+    this.gameId = gameStatic.getId();
     this.title = gameStatic.getTitle();
     this.thumbnail = gameStatic.getThumbnail();
     this.price = gameStatic.getPrice();
     if (gameDynamic != null) {
-      this.lowest_price = gameDynamic.getLowest_price();
+      this.lowestPrice = gameDynamic.getLowest_price();
     }
   }
 }
