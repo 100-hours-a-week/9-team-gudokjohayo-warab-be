7acf56420257ea34119d632e01b8107faa9d82a1
groom <hayaon0603@naver.com>
2025-04-15 17:49:05 +0900

    feat: 기본 생성자 추가

--- src/main/java/store/warab/dto/CommentRequest.java
+++ src/main/java/store/warab/dto/CommentRequest.java
@@ -2,17 +2,21 @@ package store.warab.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentRequest {
   private String content;
 
   public String getContent() {
-    return content;
+
+      return content;
   }
 
   public void setContent(String content) {
-    this.content = content;
+
+      this.content = content;
   }
 }
